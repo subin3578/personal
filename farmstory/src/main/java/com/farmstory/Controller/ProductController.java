fefcc3b2e75082a5654b92d715b5a78795ea4e08
subin3578 <ghkdtnqls95@gmail.com>
2024-09-12 16:44:18 +0900

    farm

--- farmstory/src/main/java/com/farmstory/Controller/ProductController.java
+++ farmstory/src/main/java/com/farmstory/Controller/ProductController.java
@@ -1,7 +1,7 @@
 package com.farmstory.Controller;
 
 import com.farmstory.dto.ProductDTO;
-import com.farmstory.service.ProductService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
